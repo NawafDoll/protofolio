1c35681218d1ecdc3e40366360c2ef1e2d81c433
Nawaf <nawaftalaat@hotmail.com>
2023-01-17 19:54:13 +0300

    Done

--- prot.js
+++ prot.js
@@ -14,3 +14,39 @@ contact2.addEventListener('click',()=>{
 x.addEventListener('click',()=>{
     id_contact.style.display='none'
 })
+
+let hedden =document.getElementById('hedden')
+let lines = document.getElementsByClassName('lines')[0]
+let menu = document.getElementsByClassName('menu')[0]
+
+hedden.addEventListener('click',function(){
+  lines.style.marginTop = '38px'  
+})
+
+
+
+hedden.addEventListener('click',function(){
+    menu.style.visibility= 'visible';
+    for(i=0;i<3;i++){
+    let line = document.querySelectorAll('.line')[i]
+    line.style.animationName = 'lines'  
+    line.style.animationDelay = '0s';
+    line.style.animationDuration = '0.6s'
+    line.style.animationFillMode= 'forwards';
+    line.style.animationTimingFunction= 'ease-in-out';
+    line.style.zIndex= '40';
+    line.style.position= 'relative';
+    }
+  })
+
+  document.getElementsByClassName('content')[0].addEventListener('click',()=>{
+    menu.style.visibility= 'hidden';
+    document.querySelectorAll('.line')[0].style.position= 'absolute';
+    document.querySelectorAll('.line')[1].style.position= 'absolute';
+    document.querySelectorAll('.line')[2].style.position= 'absolute';
+    document.querySelectorAll('.line')[0].style.display = 'none'
+    document.querySelectorAll('.line')[1].style.display = 'none'
+    document.querySelectorAll('.line')[2].style.display = 'none'
+    // hedden.style.width = '0px'
+    document.querySelector('#lines').style.width = '0px'
+  })
